e5a65a70057da00b00c3ac467c87aab816b58fbf
bgendell <benjamin.gendell.16@cnu.edu>
2019-11-02 17:15:27 -0400

    finish project

--- FileIO.cpp
+++ FileIO.cpp
@@ -18,12 +18,18 @@ using namespace std;
 //COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	fstream mystream;
-	mystream.open(filename.c_str(),ios_base::in);
-	if(mystream.is_open()){
+	fstream fileIn;
+	fileIn.open(filename.c_str(),ios_base::in);
+	if(fileIn.is_open()){
+		std::string temp;
+		while(!fileIn.eof()){
+			getline(fileIn,temp);
+			contents.append(temp);
+		}
+		fileIn.close();
 		return SUCCESS;
 	}
-	return COULD_NOT_OPEN_FILE_TO_WRITE;
+	return COULD_NOT_OPEN_FILE_TO_READ;
 }
 
 //trys to open filename, and serialize  myEntryVector contents to it
@@ -36,8 +42,10 @@ int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::str
 	fileOut.open(filename.c_str(),ios_base::out);
 	if(fileOut.is_open()){
 		for(int i = 0; i < myEntryVector.size(); i++){
-			fileOut >> myEntryVector[i];
+			fileOut << myEntryVector[i];
+			fileOut << "\n";
 		}
+		fileOut.close();
 		return SUCCESS;
 	}
 	return COULD_NOT_OPEN_FILE_TO_WRITE;
